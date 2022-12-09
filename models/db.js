b4110faf6aae58a3913954011d6a06d519dc66d6
JoyalJohnson98 <110291375+JoyalJohnson98@users.noreply.github.com>
2022-12-09 22:42:30 +0530

    Delete models directory

--- models/db.js
+++
@@ -1,9 +0,0 @@
-const {Sequelize} = require('sequelize');
-
-const sequelize = new Sequelize("cabworld","root","joyaljohnson",{
- host:"localhost",
- dialect:"mysql"
- 
-});
-
-module.exports=sequelize ;
